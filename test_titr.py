169d4204267f95ea881d0b04d0dd4773b176ff4e
Blair Frandeen <blairfrandeen@gmail.com>
2022-05-31 21:12:07 -0700

    test: started unit tests for parse command. Failing.

---
+++ test_titr.py
@@ -0,0 +1,45 @@
+import pytest
+import titr
+
+def test_parse():
+    invalid_args = [
+        None,
+        0,
+        1.2,
+        [1, 2, 3],
+        {1: 'one'},
+    ]
+    invalid_commands = [
+        "help, I'm a bug",
+        '-53',
+        '.25*923',
+        #  'Y',
+        '.25 4 q',
+        '.5 g g',
+    ]
+    valid_commands = [
+        'C',
+        'c',
+        '2',
+        '43',
+        '2 2',
+        '2 i',
+        '.5 3 g',
+        '.5 g 3',
+        '.25 5 "daily stand-up"',
+        '.25 5 g "group meeting"',
+        '.25"group meeting"5 g ',
+        '.25"group meeting" g 5 ',
+        ".25 5 'daily stand-up'",
+    ]
+    for arg in invalid_args:
+        with pytest.raises(TypeError):
+            titr.parse_command(arg)
+
+    for cmd in invalid_commands:
+        with pytest.raises(ValueError):
+            titr.parse_command(cmd)
+
+
+    titr.parse_command(valid_commands[0])
+    assert 0
